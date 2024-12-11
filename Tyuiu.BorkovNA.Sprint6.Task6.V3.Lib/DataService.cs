08d2416b88a9dd069971e96d88c36d0df358ae8c
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-12-12 04:59:49 +0500

    87

--- Tyuiu.BorkovNA.Sprint6.Task6.V3.Lib/DataService.cs
+++ Tyuiu.BorkovNA.Sprint6.Task6.V3.Lib/DataService.cs
@@ -1,7 +1,41 @@
-﻿namespace Tyuiu.BorkovNA.Sprint6.Task6.V3.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
+namespace Tyuiu.BorkovNA.Sprint6.Task6.V3.Lib
+{
+    public class DataService : ISprint6Task6V3
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string resStr = "";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains("w"))
+                    {
+                        resStr = resStr + " " + line;
+                    }
+                }
+            }
+            string[] FinalResult = resStr.Split(' ');
+            resStr = "";
+            for (int i = 0; i < FinalResult.Length; i++)
+            {
+                if (FinalResult[i].Contains("r"))
+                {
+                    if (resStr == "")
+                    {
+                        resStr = FinalResult[i];
+                    }
+                    else
+                    {
+                        resStr = resStr + " " + FinalResult[i];
+                    }
+                }
+            }
+            return resStr;
+        }
     }
 }
---
+++ Tyuiu.BorkovNA.Sprint6.Task6.V3.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BorkovNA.Sprint6.Task6.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
